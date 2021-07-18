c05171071b002ec155e93d34fb066b33100aea7d
BoyerDamien <dboyer@student.42.fr>
2021-07-18 15:03:59 +0200

    clean

--- exemples/main.go
+++ exemples/main.go
@@ -8,49 +8,10 @@ import (
 )
 
 func main() {
-
 	app := gapi.New(sqlite.Open("test.db"), gapi.Config{})
 	app.StaticFolders("./staticfiles")
 
 	coll := app.Collection("/api/v1")
 	coll.AddRessources(&Tag{})
 	log.Fatal(app.Listen(":3000"))
-
-	/*if err := config.InitStaticFiles(); err != nil {
-		panic(err)
-	}
-
-	// Init database && migrate models
-	log.Println("Connecting to database...")
-	if config.ENV == "PROD" {
-		db, err = gorm.Open(mysql.Open(config.DB_PROD_DSN), &gorm.Config{})
-	} else {
-		db, err = gorm.Open(sqlite.Open(config.DB_TEST_NAME), &gorm.Config{})
-	}
-	if err != nil {
-		panic(err)
-	}
-	if err := models.MigrateAll(db); err != nil {
-		panic(err)
-	}*/
-
-	/*
-		// Init app
-		test, err := database.Open(sqlite.Open("test.db"), &database.Config{})
-		app := fiber.New()
-		api := app.Group("/api")
-		v1 := api.Group("/v1")
-
-		// Middlewares
-		middlewares.InitAll(app)
-
-		// Endpoints
-		endpoints.Setup(v1, db,
-			&models.User{},
-			&models.Media{},
-			&models.Offer{},
-			&models.Portfolio{},
-			&models.Tag{},
-		)
-		log.Fatal(app.Listen(fmt.Sprintf(":%s", config.LISTEN_PORT)))*/
 }
--- go.mod
+++ go.mod
@@ -7,7 +7,6 @@ require (
 	github.com/go-playground/validator v9.31.0+incompatible
 	github.com/gofiber/fiber/v2 v2.15.0
 	github.com/leodido/go-urn v1.2.1 // indirect
-	golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97 // indirect
 	gopkg.in/go-playground/assert.v1 v1.2.1 // indirect
 	gorm.io/driver/mysql v1.1.1
 	gorm.io/driver/postgres v1.1.0
